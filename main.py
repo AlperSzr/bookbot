bdac09ae0bbc4465b79b0b21875bcf9b4d543750
AlperSzr <asezer2021@gmail.com>
2025-10-27 06:30:10 +0100

    my first project in all its glory

--- main.py
+++ main.py
@@ -4,12 +4,32 @@ def get_book_text(path):
         book_text = f.read()
     return book_text 
 
-from stats import get_num_words
+from stats import get_num_words, count_characters, dictionary_to_list, sort_on
+import sys
 
 def main():
 
-    text=get_book_text("./books/frankenstein.txt")
-    length=get_num_words(text)
-    print(f"Found", length, "total words")
+    if len(sys.argv) <= 1:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
+    else:
+        
+        path_to_book = sys.argv[1]
+        text = get_book_text(path_to_book)
+        length = get_num_words(text)
+        dict_characters = count_characters(text)
+        sorted_list = dictionary_to_list(dict_characters)
 
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {path_to_book}...")
+        print("----------- Word Count ----------")
+        print(f"Found", length, "total words")
+        print("----------- Character Count ----------")
+        for item in sorted_list:
+            if item["char"].isalpha() == True:
+                print(f"{item["char"]}: {item["num"]}")
+            else:
+                continue
+        print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -4,15 +4,21 @@ def get_num_words(text):
     return length
 
 def count_characters(text):
-    dict_characters={
-    }
-    lowered_text=text.lower()
-    for character in lowered_text:
+    dict_characters={}
+    text_lower = text.lower()
+    for character in text_lower:
         if character not in dict_characters:
-	    dict_characters[character] = 1
-	else:
-	    dict_characters[character] += 1
+            dict_characters[character] = 1
+        else:
+            dict_characters[character] += 1
     return dict_characters
 
+def sort_on(items):
+    return items["num"]
 
-count_characters("AA a!")
+def dictionary_to_list(dictionary):
+    list_of_key_and_value=[]
+    for key in dictionary:
+        list_of_key_and_value.append({"char":key, "num":dictionary[key]})
+    list_of_key_and_value.sort(reverse=True, key=sort_on)
+    return list_of_key_and_value
